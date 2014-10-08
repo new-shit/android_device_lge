cb110b43fb9acf0216d444860426c9471041237d
root <root@44362d7c44.server.com>
2014-10-08 07:48:12 -0400

    liquify

--- e975/BoardConfig.mk
+++ e975/BoardConfig.mk
@@ -20,6 +20,8 @@ include device/lge/gproj-common/BoardConfigCommon.mk
 
 BOARD_KERNEL_CMDLINE := vmalloc=600M console=null lpj=67677 user_debug=31 msm_rtb.filter=0x0 ehci-hcd.park=3 coresight-etm.boot_enable=0 androidboot.hardware=geehrc
 TARGET_KERNEL_CONFIG := f93_e975_defconfig
+TARGET_GCC_VERSION_ARM := 4.7-sm
+TARGET_GCC_VERSION_AND := 4.7-sm
 
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
